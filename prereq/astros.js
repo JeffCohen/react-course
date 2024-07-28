3d0663616adca137bc374dd978090b80770f1893
Jeff Cohen <cohen.jeff@gmail.com>
2024-07-28 18:07:14 -0500

    Start the party

---
+++ prereq/astros.js
@@ -0,0 +1,11 @@
+// Display the names of all of the astronauts in space, in alphabetic order by first name.
+
+// Your code goes here:
+
+document.addEventListener("DOMContentLoaded", function() {
+  document.querySelector("h1").addEventListener("click", getAstronauts)
+})
+
+async function getAstronauts(e) {
+  
+}
---
+++ prereq/gps.js
@@ -0,0 +1,15 @@
+// Your code goes here
+const MAPS_API_KEY="AIzaSyBloBOCFMz5PFopBpLDiKCyhTuSR-yuOzM"
+
+function onDocumentLoaded() {
+  document.querySelector("#get-location").addEventListener("click", function(e) {
+    e.preventDefault();
+  })
+}
+document.addEventListener("DOMContentLoaded", onDocumentLoaded)
+
+function updateMap(latitude, longitude) {
+  const new_src = "https://maps.googleapis.com/maps/api/staticmap?center=" + latitude + "," + longitude + "&zoom=17&size=600x400&format=png32&maptype=satellite&key=" + MAPS_API_KEY
+  document.querySelector("img").src = new_src
+}
+
